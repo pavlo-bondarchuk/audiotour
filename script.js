c4b07d203b529558b1302e3839049e89e6e6edc4
Pavlo Bondarchuk <bonddesign85@gmail.com>
2024-05-10 21:19:16 +0300

    add header

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-// interact on dom load
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM is ready');
+    document.querySelector('.navbar-toggler').addEventListener('click', function () {
+        document.querySelector('.navbar-toggler').classList.toggle('active');
+    });
+    if (window.innerWidth < 768) {
+        document.querySelector('.navbar-toggler').addEventListener('click', function () {
+            document.querySelector('.menu-mobile').classList.toggle('active');
+        });
+    }
 });
