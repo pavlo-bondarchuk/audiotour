606a58376ccc9ffb8c31691e2903b7b258545fb2
Pavlo Bondarchuk <bonddesign85@gmail.com>
2024-05-16 12:50:20 +0300

    mobile

--- script.js
+++ script.js
@@ -7,5 +7,12 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('.navbar-toggler').addEventListener('click', function () {
             document.querySelector('.menu-mobile').classList.toggle('active');
         });
+        const swiper = new Swiper('.functions--grid', {
+            loop: true,
+            slidesPerView: 1.5,
+            sliderPerSlide: 1,
+            spaceBetween: 13,
+            centeredSlides: true,
+        });
     }
 });
